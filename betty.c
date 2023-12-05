3cc1a0a17bde47bbfce512876f6cc73a209eeedb
iangongo <ianmwanyaegongo@gmail.com>
2023-12-05 05:23:25 -0800

    this is betty checks to betty.c

--- betty.c
+++ betty.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * 
  * main - Entry point of the program
- * 
- * Return: 0 success
+ * Description: This is the main function that
+ * prints a welcome message
+ *
+ * Return: Always 0 success
 */
 int main(void)
 {
 
-    printf("Welcome, Betty!\n");
-    return (0);
+	printf("Welcome, Betty!\n");
+	return (0);
 }
