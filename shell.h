8698e647f63a3dbbac2c781910545e391f5b2395
iangongo <ianmwanyaegongo@gmail.com>
2023-12-13 15:40:43 -0800

    addition of environ.c and necessary changes to shell.h

--- shell.h
+++ shell.h
@@ -23,8 +23,15 @@ typedef struct info_s
 	char **argv;
 	int line_count;
 	int readfd; // TODO: Define the data type for readfd
+/* Add the env member to info_t structure */
+	struct list_s *env;
 } info_t;
-
+/* Define structure for singly linked list */
+typedef struct list_s
+{
+    char *str;
+    struct list_s *next;
+} list_t;
 /* Initialize an instance of the info_t structure */
 #define INFO_INIT {NULL, NULL, 0, 0}
 
@@ -34,6 +41,12 @@ void print_error(info_t *info, char *estr);
 int print_d(int input, int fd);
 char *convert_number(long int num, int base, int flags);
 void remove_comments(char *buf);
+/* Function prototypes for environ.c */
+int _myenv(info_t *info);
+char *_getenv(info_t *info, const char *name);
+int _mysetenv(info_t *info);
+int _myunsetenv(info_t *info);
+int populate_env_list(info_t *info);
 
 /* Function prototypes for main.c */
 void display_prompt(void);
