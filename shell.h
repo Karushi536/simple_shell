35410beaff68293edfa48d7ef16c8aaad450167d
iangongo <ianmwanyaegongo@gmail.com>
2023-12-13 15:22:40 -0800

    errrors1.c and necessary additions to shell.h

--- shell.h
+++ shell.h
@@ -8,7 +8,34 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <limits.h> 
+#include <fcntl.h>
+#define STDERR_FILENO 2
 
+/* Define flags for convert_number */
+#define CONVERT_UNSIGNED 1
+#define CONVERT_LOWERCASE 2
+
+/* Define structure for info */
+typedef struct info_s
+{
+	char *fname;
+	char **argv;
+	int line_count;
+	int readfd; // TODO: Define the data type for readfd
+} info_t;
+
+/* Initialize an instance of the info_t structure */
+#define INFO_INIT {NULL, NULL, 0, 0}
+
+/* Function prototypes for errors1.c */
+int _erratoi(char *s);
+void print_error(info_t *info, char *estr);
+int print_d(int input, int fd);
+char *convert_number(long int num, int base, int flags);
+void remove_comments(char *buf);
+
+/* Function prototypes for main.c */
 void display_prompt(void);
 void user_input(char *command, size_t size);
 void execute_command(const char *command);
