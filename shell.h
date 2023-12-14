a504074608265fbe60a37626b99c8c5a6b19dbf0
iangongo <ianmwanyaegongo@gmail.com>
2023-12-13 18:22:34 -0800

    addition of uncompiled files

--- shell.h
+++ shell.h
@@ -24,6 +24,8 @@ typedef struct info_s
 	int line_count;
 	int readfd; // TODO: Define the data type for readfd
 /* Add the env member to info_t structure */
+	int status;
+	int err_num;
 	struct list_s *env;
 } info_t;
 /* Define structure for singly linked list */
@@ -34,8 +36,7 @@ typedef struct list_s
 } list_t;
 /* Initialize an instance of the info_t structure */
 #define INFO_INIT {NULL, NULL, 0, 0}
-
-/* Function prototypes for errors1.c */
+ Function prototypes for errors1.c */
 int _erratoi(char *s);
 void print_error(info_t *info, char *estr);
 int print_d(int input, int fd);
@@ -47,6 +48,10 @@ char *_getenv(info_t *info, const char *name);
 int _mysetenv(info_t *info);
 int _myunsetenv(info_t *info);
 int populate_env_list(info_t *info);
+/* Function prototypes for builtin.c */
+int _myexit(info_t *info);
+int _mycd(info_t *info);
+int _myhelp(info_t *info);
 
 /* Function prototypes for main.c */
 void display_prompt(void);
@@ -55,4 +60,63 @@ void execute_command(const char *command);
 char *read_line(void);
 char **split_line(char *line);
 void execute(char **args);
+/* Function declarations for getenv.c */
+char **get_environ(info_t *info);
+int _unsetenv(info_t *info, char *var);
+int _setenv(info_t *info, char *var, char *value);
+/* realloc.c */
+char *_memset(char *s, char b, unsigned int n);
+void ffree(char **pp);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+/* Function declarations for lists.c */
+list_t *add_node(list_t **head, const char *str, int num);
+list_t *add_node_end(list_t **head, const char *str, int num);
+size_t print_list_str(const list_t *h);
+int delete_node_at_index(list_t **head, unsigned int index);
+void free_list(list_t **head_ptr);
+/* Function declarations for getLine.c */
+ssize_t input_buf(info_t *info, char **buf, size_t *len);
+ssize_t get_input(info_t *info);
+ssize_t read_buf(info_t *info, char *buf, size_t *i);
+int _getline(info_t *info, char **ptr, size_t *length);
+void sigintHandler(__attribute__((unused))int sig_num);
+/* Function declarations for history.c */
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+int read_history(info_t *info);
+int build_history_list(info_t *info, char *buf, int linecount);
+int renumber_history(info_t *info);
+/* Function declarations for memory.c */
+int bfree(void **ptr);
+/* Function declarations for string1.c */
+char *_strcpy(char *dest, const char *src);
+char *_strdup(const char *str);
+void _puts(const char *str);
+int _putchar(char c);
+/* Function declarations for string.c */
+int _strlen(const char *s);
+int _strcmp(const char *s1, const char *s2);
+char *starts_with(const char *haystack, const char *needle);
+char *_strcat(char *dest, const char *src);
+/* Function declarations for builtin1.c */
+int _myhistory(info_t *info);
+int unset_alias(info_t *info, char *str);
+int set_alias(info_t *info, char *str);
+int print_alias(list_t *node);
+int _myalias(info_t *info);
+
+#define BUF_FLUSH -1
+#define WRITE_BUF_SIZE 1024
+
+typedef struct info
+{
+	int readfd;
+	/* Add other members as needed */
+} info_t;
+
+int interactive_mode(info_t *info);
+int is_delimiter(char c, char *delimiters);
+int is_alpha(int c);
+int _atoi(char *s);
+
 #endif
